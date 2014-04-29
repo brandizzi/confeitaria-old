ed8aa49d7c292b7bed1a19b7319f265535d99560
Adam Victor Nazareth Brandizzi <brandizzi@gmail.com>
2014-04-29 06:41:23 -0300

    Testing eetrieving child page

--- confeitaria/tests/page/page.py
+++ confeitaria/tests/page/page.py
@@ -58,3 +58,37 @@ class PageTestCase(unittest.TestCase):
         root.leaf = LeafPage()
 
         self.assertEqual('/root/leaf/', root.leaf.path)
+
+    def test_get_child_page_content_through_http(self):
+        class RootPage(Page):
+            @cherrypy.expose
+            def index(self):
+                return 'root'
+        class LeafPage(Page):
+            @cherrypy.expose
+            def index(self):
+                return 'leaf'
+
+        root = RootPage()
+        root.leaf = LeafPage()
+
+        p = Process(target=root.run)
+        try:
+            p.start()
+            time.sleep(0.5)
+
+            result = requests.get('http://localhost:8080')
+
+            self.assertEqual(200, result.status_code)
+            self.assertEqual('root', result.text)
+
+            result = requests.get('http://localhost:8080/leaf')
+
+            self.assertEqual(200, result.status_code)
+            self.assertEqual('leaf', result.text)
+        except AssertionError:
+            raise
+        except Exception as e:
+            self.fail(e)
+        finally:
+            p.terminate()
