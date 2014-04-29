8a10b06c9765235956c4477617eaa06d852c8445
Adam Victor Nazareth Brandizzi <brandizzi@gmail.com>
2014-04-28 23:20:00 -0300

    Passing the name of a module to ResourcePage

--- confeitaria/page/resource.py
+++ confeitaria/page/resource.py
@@ -5,12 +5,14 @@ import cherrypy
 from .page import Page
 
 class ResourcePage(Page):
-    def __init__(self, directory, *args, **kwargs):
+    def __init__(self, root, directory, *args, **kwargs):
         Page.__init__(self, *args, **kwargs)
+
+        self.root = root
         self.directory = directory
 
     @cherrypy.expose
     def index(self, resource, *args, **kwargs):
         resource_path = os.path.join(self.directory, resource)
 
-        return pkgutil.get_data(self.__class__.__module__, resource_path)
+        return pkgutil.get_data(self.root, resource_path)
--- confeitaria/tests/page/resource.py
+++ confeitaria/tests/page/resource.py
@@ -10,9 +10,6 @@ import requests
 class ResourcePageTestCase(unittest.TestCase):
 
     def test_serve_resource(self):
-        class TestResourcePage(ResourcePage):
-            pass
-
         txt_content = pkgutil.get_data(__name__, 'txt/test.txt')
-        resource_page = TestResourcePage(directory='txt')
+        resource_page = ResourcePage(root=__name__, directory='txt')
         self.assertEqual(txt_content, resource_page.index('test.txt'))
