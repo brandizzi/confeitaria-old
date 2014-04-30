0d0929904d72e5e76c6f4b19570ac30fd2829146
Adam Victor Nazareth Brandizzi <brandizzi@gmail.com>
2014-04-29 22:29:34 -0300

    ResourcePage can receive content-type as parameter

--- confeitaria/page/resource.py
+++ confeitaria/page/resource.py
@@ -5,14 +5,20 @@ import cherrypy
 from .page import Page
 
 class ResourcePage(Page):
-    def __init__(self, root, directory, *args, **kwargs):
+
+    def __init__(self, root, directory, content_type='text/plain',
+            *args, **kwargs):
+
         Page.__init__(self, *args, **kwargs)
 
         self.root = root
         self.directory = directory
+        self.content_type = content_type
 
     @cherrypy.expose
     def index(self, resource, *args, **kwargs):
+        cherrypy.response.headers['Content-Type']= self.content_type
+
         resource_path = os.path.join(self.directory, resource)
 
         return pkgutil.get_data(self.root, resource_path)
--- confeitaria/tests/page/resource.py
+++ confeitaria/tests/page/resource.py
@@ -1,6 +1,7 @@
 import time
 import pkgutil
 import unittest2 as unittest
+from multiprocessing import Process
 
 from confeitaria import ResourcePage
 
@@ -13,3 +14,25 @@ class ResourcePageTestCase(unittest.TestCase):
         txt_content = pkgutil.get_data(__name__, 'txt/test.txt')
         resource_page = ResourcePage(root=__name__, directory='txt')
         self.assertEqual(txt_content, resource_page.index('test.txt'))
+
+    def test_set_content_type(self):
+        resource_page = ResourcePage(
+            root=__name__, directory='css', content_type='text/css'
+        )
+
+        p = Process(target=resource_page.run)
+        try:
+            p.start()
+            time.sleep(0.5)
+
+            result = requests.get('http://localhost:8080/test.css')
+
+            self.assertEqual(200, result.status_code)
+            content_type = result.headers['content-type']
+            self.assertTrue(content_type.startswith('text/css'))
+        except AssertionError as e:
+            raise
+        except Exception as e:
+            self.fail(e)
+        finally:
+            p.terminate()
